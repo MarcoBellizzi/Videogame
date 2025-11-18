1e64fc46a76cef2f8477cab851448269d1d37a41
Marco <marcobellizzi96@gmail.com>
2025-11-18 11:25:51 +0100

    uploaded files with gitignore

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Transform playerOrientation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        playerOrientation = GameObject.Find("PlayerOrientation").transform;
+    }
+
+    void Update()
+    {
+        // rotate orientation of the player
+        playerOrientation.forward = (playerOrientation.position - new Vector3(transform.position.x, playerOrientation.position.y, transform.position.z)).normalized;
+    }
+
+}
